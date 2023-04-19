33376a3e9a3bd8279851e9165d79eb1add5a1108
coderblack <coderblack@163.com>
2023-04-19 18:26:28 +0800

    deep as sea

--- akka-rpc/src/main/java/cn/doitedu/deepsea/my_rpc/ClientInvocationHandler.java
+++ akka-rpc/src/main/java/cn/doitedu/deepsea/my_rpc/ClientInvocationHandler.java
@@ -2,6 +2,9 @@ package cn.doitedu.deepsea.my_rpc;
 
 import akka.actor.ActorRef;
 import akka.actor.ActorSelection;
+import akka.pattern.Patterns;
+import org.apache.flink.runtime.concurrent.akka.AkkaFutureUtils;
+import scala.concurrent.Future;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
@@ -19,8 +22,12 @@ public class ClientInvocationHandler implements InvocationHandler {
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-
         Message message = new Message("a.b.class", method.getName(), args);
+
+        /*Future<Object> future = Patterns.ask(targetRef, message, 1000L);
+        String o = (String) AkkaFutureUtils.toJava(future).get();*/
+
+
         targetRef.tell(message,selfRef);
 
         return null;
